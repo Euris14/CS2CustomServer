d8efcc22c664ab813b7bd2103a7251ab463a2edc
Euris <euris1312@gmail.com>
2023-10-04 02:48:47 -0400

    formatting

--- server.py
+++ server.py
@@ -1,15 +1,18 @@
+"""Controls cs2 server"""
 #! /usr/bin/python3
 import os
-import json
-import sys, getopt
+import sys
+import getopt
 from steamcmd import steamcmd
 
-cpath = r"config/cs2.json"
-config = steamcmd.load_config(cpath)
+CONFIG_PATH = r"config/cs2.json"
+config = steamcmd.load_config(CONFIG_PATH)
 
 serverdir = config['steamcmd']['options']['installDir']
 
 def main(argv): # Runs user inputs and deploys functions
+    """ This is the main function, which runs all functions and collects
+    user input."""
     choice = ''
     opts, args = getopt.getopt(argv, "hc:",["choice="])
     for opt, arg in opts: # gets command and argument.
@@ -18,48 +21,40 @@ def main(argv): # Runs user inputs and deploys functions
             sys.exit(5)
         elif opt in ('-c', "--choice"):
             choice = arg
-            
     match choice.lower(): # Check user input.
         case "install":
             choice = input("Are you sure you would like to " +
                            "install?[y/n] ").lower()
-    
             if choice == ("y") or choice == ("yes"):
-                InstallServer()
+                install_server()
             else:
                 print("Quitting...")
                 sys.exit(2)
-                
         case "validate":
+            print("Be cautious with this on as it can rewrite some custom server files!")
             choice = input("Are you sure you would like to " +
                            "validate the server files?[y/n] ").lower()
-            
             if choice == ("y") or choice == ("yes"):
-                ValidateServer()
+                validate_server()
             else:
                 print("Quitting...")
                 sys.exit(2)
-                
         case "update":
             choice = input("Are you sure you would like to " +
                            "update the server?[y/n] ").lower()
-            
             if choice == ("y") or choice == ("yes"):
-                UpdateServer()
+                update_server()
             else:
                 print("Quitting...")
                 sys.exit(2)
-                
         case "start":
             choice = input("Are you sure you would like to " +
                            "start the server?[y/n] ").lower()
-            
             if choice == ("y") or choice == ("yes"):
-                StartGame()
+                start_server()
             else:
                 print("Quitting...")
                 sys.exit(2)
-                
         case _: # Displays valid functions, if none inputed.
             print('./server.py -c <choice>')
             functions = ['install', 'update', 'validate', 'start']
@@ -67,8 +62,8 @@ def main(argv): # Runs user inputs and deploys functions
             for i in range(len(functions)):
                 print(f"{i + 1}. ./server.py -c {functions[i]}")
             sys.exit(5)
-        
-def ServerExist(): # Check if server is already installed.
+def server_exist(): # Check if server is already installed.
+    """ This function checks the serverdir for an existing server. """
     file_exist = os.path.exists(serverdir)
     if file_exist:
         print(f"Server is already installed. In order to update the server, " +
@@ -76,21 +71,23 @@ def ServerExist(): # Check if server is already installed.
         sys.exit(2)
     else:
         return True
-    
-def InstallServer(): # Install game server only if dir /serverfiles/ does not exist.
-    if ServerExist():
-        steamcmd.update(cpath)
-    
-def UpdateServer(): # Updates the game server!
+def install_server(): # Install game server only if dir /serverfiles/ does not exist.
+    """ This function installs the server to the server path in the config file."""
+    if server_exist():
+        steamcmd.update(CONFIG_PATH)
+def update_server():# Updates the game server!
+    """ This function updates the server. """
     print("Updating...")
-    steamcmd.update(cpath)
-    
-def ValidateServer(): # Validate the game server!
+    steamcmd.update(CONFIG_PATH)
+def validate_server():
+    """ This function validates the server paths and changes each one back
+    to the server original. """
     print("Validating game files!!!!")
-    steamcmd.validate(cpath)
-    
-def StartGame(): # Start the game server!
-    os.system(rf"{serverdir} -dedicated -usercon +map +game_type 0 +game_mode 1 "
-              + f"+map de_dust2 +sv_setsteamaccount {config['cs2']['steamtoken']} +clientport 27015")
+    steamcmd.validate(CONFIG_PATH)
+def start_server():
+    """ This function starts the server in this case its cs2. """
+    os.system(rf"{serverdir} -dedicated -usercon +map +game_type 0 +game_mode 1 "+
+                f"+map de_dust2 +sv_setsteamaccount {config['cs2']['steamtoken']} "+
+                "+clientport 27015")
 if __name__ == "__main__":
     main(sys.argv[1:])
--- steamcmd/steamcmd.py
+++ steamcmd/steamcmd.py
@@ -1,3 +1,4 @@
+""" This is my way of using steamcmd with python. """
 import os
 import json
 
@@ -20,46 +21,50 @@ DEFAULT_CONFIG = {
     {
         "steamtoken" : "STEAMTOKENHERE",
         "gameserverpath" : "/home/cs2/serverfiles/game/bin/linuxsteamrt64/cs2"
-        
     }
 }
 
 def load_config(cpath): # Looks for config file, if not found then create a default one.
+    """ Looks for the default config file, if one can not be found
+    then it creates a new one. """
     try:
-        with open(cpath) as f:
-            return json.load(f)
+        with open(cpath) as config:
+            return json.load(config)
     except FileNotFoundError:
         print(f"Config file not found at {cpath}, creating a new one...")
         save_config(DEFAULT_CONFIG, cpath)
         return DEFAULT_CONFIG
-    
-
 def save_config(def_config, cpath):
+    """ This function saves the new default config to the default config dir """
     with open(cpath, 'w') as config_file:
         json.dump(def_config, config_file, indent=4)
-
 def update(cpath): # Update CS:2 Server with steamcmd.
-    config = load_config(cpath) 
+    """ Updates or installs server using steamcmd. """
+    config = load_config(cpath)
     steamcmd = config['steamcmd']
     options = steamcmd['options']
-    
     if options['login']:
-        os.system(f"steamcmd +force_install_dir {options['installDir']} +login {steamcmd['auth']['user']} {steamcmd['auth']['pass']} +app_update {options['appID']} +quit")
+        os.system(f"steamcmd +force_install_dir {options['installDir']} " +
+                  f"+login {steamcmd['auth']['user']} " +
+                  f"{steamcmd['auth']['pass']} +app_update {options['appID']} +quit")
     elif not options['login']:
         try:
-            os.system(f"steamcmd +force_install_dir {options['installDir']} +login anonymous +app_update {options['appID']} +quit")
+            os.system(f"steamcmd +force_install_dir {options['installDir']} +login anonymous " +
+                      f"+app_update {options['appID']} +quit")
         except:
             print("Game server probably needs a login, change cs2.json.")
-            
 def validate(cpath): #Validates the cs2 server.
-    config = load_config(cpath) 
+    """ **** Validates serverdir against steam default files and replaces them (I think?)"""
+    config = load_config(cpath)
     steamcmd = config['steamcmd']
     options = steamcmd['options']
-    
     if options['login']:
-        os.system(f"steamcmd +force_install_dir {options['installDir']} +login {steamcmd['auth']['user']} {steamcmd['auth']['pass']} +app_update {options['appID']} validat +quit")
+        os.system(f"steamcmd +force_install_dir {options['installDir']} " +
+                  f"+login {steamcmd['auth']['user']} {steamcmd['auth']['pass']} " +
+                  f"+app_update {options['appID']} validate +quit")
     elif not options['login']:
         try:
-            os.system(f"steamcmd +force_install_dir {options['installDir']} +login anonymous +app_update {options['appID']} validate +quit")
+            os.system(f"steamcmd +force_install_dir {options['installDir']} +login anonymous" +
+                      f" +app_update {options['appID']} validate +quit")
         except:
             print("Game server probably needs a login, change cs2.json.")
