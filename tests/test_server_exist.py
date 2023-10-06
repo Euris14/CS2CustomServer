b554a0e5644e63869fb2f23929e552770cf3e264
Euris <euris1312@gmail.com>
2023-10-05 21:32:04 -0400

    making pylint happy

--- tests/test_server_exist.py
+++ tests/test_server_exist.py
@@ -1,7 +1,5 @@
 import os
 def test_server_exist():
-    
-    print(usr_dir)
     usr_dir = os.path.expanduser('~')
     file_exist = os.path.exists(fr'{usr_dir}/serverfiles')
     assert file_exist
